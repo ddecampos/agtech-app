6f25b6bcae4212a83f883285ee0734cea3fe6d4b
ddecampos <danieldecampos.v@gmail.com>
2024-09-25 17:18:40 -0300

    Add test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,7 +1,2 @@
-# tests/test_api.py
-import requests
-
-def test_api_status():
-    response = requests.get("https://openweathermap.org/api")
-    assert response.status_code == 200
-    assert response.json() == {"status": "ok"}
+def test_example():
+    assert 1 + 1 == 2
